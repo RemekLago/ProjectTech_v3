6463c181ae61d5f3eccc382685bf364aab0809b9
Remeklago <remigiusz.lagodzki@gmail.com>
2022-11-24 18:17:08 +0100

    Added RecyclerViewInterface3 and RecyclerViewInterface4

---
+++ app/src/main/java/v3/projecttech_v3/RecyclerViewInterface3.java
@@ -0,0 +1,6 @@
+package v3.projecttech_v3;
+
+public interface RecyclerViewInterface3 {
+
+    void onItemClick3(int position);
+}
---
+++ app/src/main/java/v3/projecttech_v3/RecyclerViewInterface4.java
@@ -0,0 +1,6 @@
+package v3.projecttech_v3;
+
+public interface RecyclerViewInterface4 {
+
+    void onItemClick3(int position);
+}
