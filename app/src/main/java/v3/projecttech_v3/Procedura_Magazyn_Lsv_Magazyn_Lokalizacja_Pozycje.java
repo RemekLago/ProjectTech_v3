20388b4aacd8a760e85c16ab164164983dbc9550
Remeklago <remigiusz.lagodzki@gmail.com>
2022-11-25 10:19:32 +0100

    added DataBaseChanges4

--- app/src/main/java/v3/projecttech_v3/Procedura_Magazyn_Lsv_Magazyn_Lokalizacja_Pozycje.java
+++ app/src/main/java/v3/projecttech_v3/Procedura_Magazyn_Lsv_Magazyn_Lokalizacja_Pozycje.java
@@ -78,25 +78,25 @@ public class Procedura_Magazyn_Lsv_Magazyn_Lokalizacja_Pozycje {
             int idNumber = 0;
             while (rs.next()) {
                 ArrayList<String> tmpRecord = new ArrayList<>();
-//                idNumber++;
-//                Data tmpInsertData = new Data( idNumber,
-//                        rs.getString(1),
-//                        rs.getString(2),
-//                        rs.getString(3),
-//                        rs.getString(4),
-//                        rs.getString(5),
-//                        rs.getString(6),
-//                        rs.getString(7),
-//                        rs.getString(8),
-//                        rs.getString(9),
-//                        rs.getString(10),
-//                        rs.getString(11),
-//                        rs.getString(12),
-//                        rs.getString(13),
-//                        rs.getString(14),
-//                        rs.getString(15)
-//                );
-//                dataBaseSQL.insertData(tmpInsertData);
+                idNumber++;
+                Data4 tmpInsertData = new Data4( idNumber,
+                        rs.getString(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getString(4),
+                        rs.getString(5),
+                        rs.getString(6),
+                        rs.getString(7),
+                        rs.getString(8),
+                        rs.getString(9),
+                        rs.getString(10),
+                        rs.getString(11),
+                        rs.getString(12),
+                        rs.getString(13),
+                        rs.getString(14),
+                        rs.getString(15)
+                );
+                dataBaseSQL4.insertData(tmpInsertData);
 
                 for (int i = 1; i <= numberOfColumns4; i++) {
                     tmpRecord.add(rs.getString(i));
