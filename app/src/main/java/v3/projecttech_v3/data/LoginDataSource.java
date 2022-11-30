614ab996b41559c316072467ff1d9c24638b1ce2
Remeklago <remigiusz.lagodzki@gmail.com>
2022-11-30 11:53:26 +0100

    added Activity Register

---
+++ app/src/main/java/v3/projecttech_v3/data/LoginDataSource.java
@@ -0,0 +1,29 @@
+package v3.projecttech_v3.data;
+
+import v3.projecttech_v3.data.model.LoggedInUser;
+
+import java.io.IOException;
+
+/**
+ * Class that handles authentication w/ login credentials and retrieves user information.
+ */
+public class LoginDataSource {
+
+    public Result<LoggedInUser> login(String username, String password) {
+
+        try {
+            // TODO: handle loggedInUser authentication
+            LoggedInUser fakeUser =
+                    new LoggedInUser(
+                            java.util.UUID.randomUUID().toString(),
+                            "Jane Doe");
+            return new Result.Success<>(fakeUser);
+        } catch (Exception e) {
+            return new Result.Error(new IOException("Error logging in", e));
+        }
+    }
+
+    public void logout() {
+        // TODO: revoke authentication
+    }
+}
