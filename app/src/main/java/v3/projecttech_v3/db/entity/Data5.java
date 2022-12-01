a843c7b5da006ad89b21829b9ff865ae51ba686f
Remeklago <remigiusz.lagodzki@gmail.com>
2022-12-01 13:30:58 +0100

    Data5 - add passwordConfirm

--- app/src/main/java/v3/projecttech_v3/db/entity/Data5.java
+++ app/src/main/java/v3/projecttech_v3/db/entity/Data5.java
@@ -3,6 +3,7 @@ package v3.projecttech_v3.db.entity;
 import net.sourceforge.jtds.jdbc.DateTime;
 
 import java.sql.Date;
+import java.time.LocalDateTime;
 
 public class Data5 {
     // for creating new User
@@ -19,35 +20,72 @@ public class Data5 {
     public static final String COLUMN_STANOWISKOID = "StanowiskoID";
     public static final String COLUMN_ADDDATE = "AddDate";
     public static final String COLUMN_STATUSID = "StatusID";
+    public static final String COLUMN_PASSWORD = "Password";
+
 
 
     private int id;
     private String imie;
     private String nazwisko;
-    private int lokalizacjaID;
     private String mail;
-    private int telefon;
-    private int dzialID;
-    private int stanowiskoID;
+    private String telefon;
+    private String lokalizacjaID;
+    private String dzialID;
+    private String stanowiskoID;
     private String addDate;
-    private int statusID;
+    private String statusID;
+    private String password;
+    private String passwordConfirm;
 
     public Data5() {
 
     }
 
-    public Data5(int id, String imie, String nazwisko, int lokalizacjaID, String mail, int telefon,
-                 int dzialID, int stanowiskoID, String addDate, int statusID) {
+    public Data5(int id, String imie, String nazwisko, String mail, String telefon,
+                 String lokalizacjaID, String dzialID, String stanowiskoID,
+                 String addDate, String statusID, String password) {
         this.id = id;
         this.imie = imie;
         this.nazwisko = nazwisko;
-        this.lokalizacjaID = lokalizacjaID;
         this.mail = mail;
         this.telefon = telefon;
+        this.lokalizacjaID = lokalizacjaID;
         this.dzialID = dzialID;
         this.stanowiskoID = stanowiskoID;
         this.addDate = addDate;
         this.statusID = statusID;
+        this.password = password;
+    }
+
+    public Data5(String imie, String nazwisko, String mail, String telefon,
+                 String lokalizacjaID, String dzialID, String stanowiskoID,
+                 String addDate, String statusID, String password) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.mail = mail;
+        this.telefon = telefon;
+        this.lokalizacjaID = lokalizacjaID;
+        this.dzialID = dzialID;
+        this.stanowiskoID = stanowiskoID;
+        this.addDate = addDate;
+        this.statusID = statusID;
+        this.password = password;
+    }
+
+    public Data5(String imie, String nazwisko, String mail, String telefon,
+                 String lokalizacjaID, String dzialID, String stanowiskoID,
+                 String addDate, String statusID, String password, String passwordConfirm) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.mail = mail;
+        this.telefon = telefon;
+        this.lokalizacjaID = lokalizacjaID;
+        this.dzialID = dzialID;
+        this.stanowiskoID = stanowiskoID;
+        this.addDate = addDate;
+        this.statusID = statusID;
+        this.password = password;
+        this.passwordConfirm = passwordConfirm;
     }
 
     public int getId() {
@@ -74,14 +112,6 @@ public class Data5 {
         this.nazwisko = nazwisko;
     }
 
-    public int getLokalizacjaID() {
-        return lokalizacjaID;
-    }
-
-    public void setLokalizacjaID(int lokalizacjaID) {
-        this.lokalizacjaID = lokalizacjaID;
-    }
-
     public String getMail() {
         return mail;
     }
@@ -90,27 +120,35 @@ public class Data5 {
         this.mail = mail;
     }
 
-    public int getTelefon() {
+    public String getTelefon() {
         return telefon;
     }
 
-    public void setTelefon(int telefon) {
+    public void setTelefon(String telefon) {
         this.telefon = telefon;
     }
 
-    public int getDzialID() {
+    public String getLokalizacjaID() {
+        return lokalizacjaID;
+    }
+
+    public void setLokalizacjaID(String lokalizacjaID) {
+        this.lokalizacjaID = lokalizacjaID;
+    }
+
+    public String getDzialID() {
         return dzialID;
     }
 
-    public void setDzialID(int dzialID) {
+    public void setDzialID(String dzialID) {
         this.dzialID = dzialID;
     }
 
-    public int getStanowiskoID() {
+    public String getStanowiskoID() {
         return stanowiskoID;
     }
 
-    public void setStanowiskoID(int stanowiskoID) {
+    public void setStanowiskoID(String stanowiskoID) {
         this.stanowiskoID = stanowiskoID;
     }
 
@@ -122,26 +160,43 @@ public class Data5 {
         this.addDate = addDate;
     }
 
-    public int getStatusID() {
+    public String getStatusID() {
         return statusID;
     }
 
-    public void setStatusID(int statusID) {
+    public void setStatusID(String statusID) {
         this.statusID = statusID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPasswordConfirm() {
+        return passwordConfirm;
+    }
+
+    public void setPasswordConfirm(String password) {
+        this.passwordConfirm = passwordConfirm;
     }
 
     public static final String CREATE_TABLE =
             "CREATE TABLE " + TABLE_NAME + " (" +
-                    COLUMN_ID + " INTEGER PRIMARY KEY AUTO_INCREMENT, " +
-                    COLUMN_IMIE + " TEXT NOT NULL, " +
-                    COLUMN_NAZWISKO + " TEXT NOT NULL, " +
+                    COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    COLUMN_IMIE + " TEXT, " +
+                    COLUMN_NAZWISKO + " TEXT, " +
+                    COLUMN_MAIL + " TEXT, " +
+                    COLUMN_TELEFON + " INTEGER, " +
                     COLUMN_LOKALIZACJAID + " INTEGER, " +
-                    COLUMN_MAIL + " TEXT NOT NULL, " +
-                    COLUMN_TELEFON + " INTEGER NOT NULL, " +
-                    COLUMN_DZIALID + " INTEGER NOT NULL, " +
-                    COLUMN_STANOWISKOID + " INTEGER NOT NULL, " +
-                    COLUMN_ADDDATE + " DATETIME NOT NULL, " +
-                    COLUMN_STATUSID + " INTEGER NOT NULL" +
+                    COLUMN_DZIALID + " INTEGER, " +
+                    COLUMN_STANOWISKOID + " INTEGER, " +
+                    COLUMN_ADDDATE + " DATETIME, " +
+                    COLUMN_STATUSID + " INTEGER, " +
+                    COLUMN_PASSWORD + " TEXT" +
                     ")";
 
 
