54bc1037512df8c71d419fc8c524ccc02b779569
Remeklago <remigiusz.lagodzki@gmail.com>
2022-12-06 18:11:40 +0100

    add Activity_Home_Lokalizacje

---
+++ app/src/main/java/v3/projecttech_v3/Activity_Home_Lokalizacje.java
@@ -0,0 +1,14 @@
+package v3.projecttech_v3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Home_Lokalizacje extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_lokalizacje);
+    }
+}
