fc0454c66209797f787ab333b4f9c9716ac6481c
Remeklago <remigiusz.lagodzki@gmail.com>
2022-12-12 20:04:44 +0100

    changes in DataBaseChanges5_home in DoinBackgroud

--- app/src/main/java/v3/projecttech_v3/DataBaseChanges5_home.java
+++ app/src/main/java/v3/projecttech_v3/DataBaseChanges5_home.java
@@ -1,16 +1,27 @@
 package v3.projecttech_v3;
 
 import android.os.AsyncTask;
+import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-public class DataBaseChanges5_home extends AsyncTask<String, Void, HashMap<String, ArrayList<String>>> {
+public class DataBaseChanges5_home extends AsyncTask<String, Void, ArrayList<ArrayList<String>>> {
+    public static ArrayList<ArrayList<String>> tmpArray;
+
 
     @Override
-    protected HashMap<String, ArrayList<String>> doInBackground(String... strings) {
+    protected ArrayList<ArrayList<String>> doInBackground(String... strings) {
 
-        return Procedura_Drzewko_Kafelki_prcAccountFormTree.takingUserSettings();
+        Procedura_Drzewko_Kafelki_prcAccountFormTree procedura = new Procedura_Drzewko_Kafelki_prcAccountFormTree();
+        tmpArray = procedura.takingUserSettings();
+//
+//        Log.i("checking", "tmpArray: " + tmpArray.size());
+//        Log.i("checking", "tmpArray: " + tmpArray.get(0));
+//        Log.i("checking", "tmpArray: " + tmpArray.get(1));
+//        Log.i("checking", "tmpArray: " + tmpArray.get(2));
+
+        return tmpArray;
     }
 }
