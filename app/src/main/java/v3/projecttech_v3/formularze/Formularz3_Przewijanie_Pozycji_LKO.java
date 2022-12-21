990ffcfaf38d7f837cb89d4b46f8928953659249
Remeklago <remigiusz.lagodzki@gmail.com>
2022-12-21 08:13:56 +0100

    added new layout and activity: - activity_formularz3_przewijanie_pozycji_lko.xml - activity_formularz4_wydanie_pozycji.xml

---
+++ app/src/main/java/v3/projecttech_v3/formularze/Formularz3_Przewijanie_Pozycji_LKO.java
@@ -0,0 +1,16 @@
+package v3.projecttech_v3.formularze;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import v3.projecttech_v3.R;
+
+public class Formularz3_Przewijanie_Pozycji_LKO extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formularz3_przewijanie_pozycji_lko);
+    }
+}
---
+++ app/src/main/java/v3/projecttech_v3/formularze/Formularz4_Wydanie_Pozycji.java
@@ -0,0 +1,16 @@
+package v3.projecttech_v3.formularze;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import v3.projecttech_v3.R;
+
+public class Formularz4_Wydanie_Pozycji extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formularz4_wydanie_pozycji);
+    }
+}
