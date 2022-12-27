f50d0a1396e715b509b546921746f6a397f66d04
Remeklago <remigiusz.lagodzki@gmail.com>
2022-12-27 19:42:32 +0100

    add new activity activity_photo.xml

---
+++ app/src/main/java/v3/projecttech_v3/Photo_Activity.java
@@ -0,0 +1,14 @@
+package v3.projecttech_v3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Photo_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo);
+    }
+}
