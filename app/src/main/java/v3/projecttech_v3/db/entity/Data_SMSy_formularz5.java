af9e8ead7a3379d6043f08825504fe613e93a260
Remeklago <remigiusz.lagodzki@gmail.com>
2023-01-03 07:14:11 +0100

    update entity Data_SMSy_formularz5

--- app/src/main/java/v3/projecttech_v3/db/entity/Data_SMSy_formularz5.java
+++ app/src/main/java/v3/projecttech_v3/db/entity/Data_SMSy_formularz5.java
@@ -1,4 +1,96 @@
 package v3.projecttech_v3.db.entity;
 
+import net.sourceforge.jtds.jdbc.DateTime;
+
 public class Data_SMSy_formularz5 {
+
+    // Constants for Database
+    public static final String TABLE_NAME = "SMSY";
+    public static final String COLUMN_ID = "Id";
+    public static final String COLUMN_NUMER = "Numer";
+    public static final String COLUMN_TRESC = "Tresc";
+    public static final String COLUMN_CZYWYSLANO = "CzyWyslano";
+    public static final String COLUMN_ADDDATE = "AddDate";
+
+    //Variables
+    private int id;
+    private int numer;
+    private String tresc;
+    private boolean czyWyslano;
+    private DateTime addDate;
+
+    // Constructor
+
+    public Data_SMSy_formularz5(int id, int numer, String tresc, boolean czyWyslano, DateTime addDate) {
+        this.id = id;
+        this.numer = numer;
+        this.tresc = tresc;
+        this.czyWyslano = czyWyslano;
+        this.addDate = addDate;
+    }
+
+    public Data_SMSy_formularz5(int numer, String tresc, boolean czyWyslano, DateTime addDate) {
+        this.numer = numer;
+        this.tresc = tresc;
+        this.czyWyslano = czyWyslano;
+        this.addDate = addDate;
+    }
+
+    public Data_SMSy_formularz5() {
+    }
+
+
+    //Getters and setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNumer() {
+        return numer;
+    }
+
+    public void setNumer(int numer) {
+        this.numer = numer;
+    }
+
+    public String getTresc() {
+        return tresc;
+    }
+
+    public void setTresc(String tresc) {
+        this.tresc = tresc;
+    }
+
+    public boolean isCzyWyslano() {
+        return czyWyslano;
+    }
+
+    public void setCzyWyslano(boolean czyWyslano) {
+        this.czyWyslano = czyWyslano;
+    }
+
+    public DateTime getAddDate() {
+        return addDate;
+    }
+
+    public void setAddDate(DateTime addDate) {
+        this.addDate = addDate;
+    }
+
+
+    // SQL QUERY: Creating the Table
+    public static final String CREATE_TABLE =
+            "CREATE TABLE " + TABLE_NAME + " (" +
+                    COLUMN_ID + " INTEGER PRIMARY KEY, " +
+                    COLUMN_NUMER + " INTEGER, " +
+                    COLUMN_TRESC + " VARCHAR, " +
+                    COLUMN_CZYWYSLANO + " BIT, " +
+                    COLUMN_ADDDATE + " DATETIME" +
+                    ")";
+
 }
