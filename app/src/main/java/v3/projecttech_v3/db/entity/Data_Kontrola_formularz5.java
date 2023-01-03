361f1e2ce713a7625bef03646309ab419881162a
Remeklago <remigiusz.lagodzki@gmail.com>
2023-01-03 07:15:23 +0100

    update entity Data_Kontrola_formularz5

--- app/src/main/java/v3/projecttech_v3/db/entity/Data_Kontrola_formularz5.java
+++ app/src/main/java/v3/projecttech_v3/db/entity/Data_Kontrola_formularz5.java
@@ -1,4 +1,167 @@
 package v3.projecttech_v3.db.entity;
 
+import net.sourceforge.jtds.jdbc.DateTime;
+
 public class Data_Kontrola_formularz5 {
+
+    // Constants for Database
+    public static final String TABLE_NAME = "KONTROLA";
+    public static final String COLUMN_ID = "Id";
+    public static final String COLUMN_MASZYNAID = "MaszynaId";
+    public static final String COLUMN_OPERATORID = "OperatorId";
+    public static final String COLUMN_PRZEWINIENIEID = "PrzewinienieId";
+    public static final String COLUMN_SMS = "SMS";
+    public static final String COLUMN_SMSNUMER = "SMSNumer";
+    public static final String COLUMN_SMSTRESC = "SMSTresc";
+    public static final String COLUMN_UWAGA = "Uwaga";
+    public static final String COLUMN_ADDUSERID = "AddUserId";
+    public static final String COLUMN_ADDDATE = "AddDate";
+
+    //Variables
+    private int id;
+    private int maszynaId;
+    private int operatorId;
+    private int przewinienieId;
+    private boolean sms;
+    private int smsNumer;
+    private String smsTresc;
+    private String  uwaga;
+    private int addUserId;
+    private DateTime addDate;
+
+
+    // Constructor
+
+    public Data_Kontrola_formularz5(int id, int maszynaId, int operatorId, int przewinienieId, boolean sms, int smsNumer, String smsTresc, String uwaga, int addUserId, DateTime addDate) {
+        this.id = id;
+        this.maszynaId = maszynaId;
+        this.operatorId = operatorId;
+        this.przewinienieId = przewinienieId;
+        this.sms = sms;
+        this.smsNumer = smsNumer;
+        this.smsTresc = smsTresc;
+        this.uwaga = uwaga;
+        this.addUserId = addUserId;
+        this.addDate = addDate;
+    }
+
+    public Data_Kontrola_formularz5(int maszynaId, int operatorId, int przewinienieId, boolean sms, int smsNumer, String smsTresc, String uwaga, int addUserId, DateTime addDate) {
+        this.maszynaId = maszynaId;
+        this.operatorId = operatorId;
+        this.przewinienieId = przewinienieId;
+        this.sms = sms;
+        this.smsNumer = smsNumer;
+        this.smsTresc = smsTresc;
+        this.uwaga = uwaga;
+        this.addUserId = addUserId;
+        this.addDate = addDate;
+    }
+
+    public Data_Kontrola_formularz5() {
+    }
+
+    //Getters and setters
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getMaszynaId() {
+        return maszynaId;
+    }
+
+    public void setMaszynaId(int maszynaId) {
+        this.maszynaId = maszynaId;
+    }
+
+    public int getOperatorId() {
+        return operatorId;
+    }
+
+    public void setOperatorId(int operatorId) {
+        this.operatorId = operatorId;
+    }
+
+    public int getPrzewinienieId() {
+        return przewinienieId;
+    }
+
+    public void setPrzewinienieId(int przewinienieId) {
+        this.przewinienieId = przewinienieId;
+    }
+
+    public boolean isSms() {
+        return sms;
+    }
+
+    public void setSms(boolean sms) {
+        this.sms = sms;
+    }
+
+    public int getSmsNumer() {
+        return smsNumer;
+    }
+
+    public void setSmsNumer(int smsNumer) {
+        this.smsNumer = smsNumer;
+    }
+
+    public String getSmsTresc() {
+        return smsTresc;
+    }
+
+    public void setSmsTresc(String smsTresc) {
+        this.smsTresc = smsTresc;
+    }
+
+    public String getUwaga() {
+        return uwaga;
+    }
+
+    public void setUwaga(String uwaga) {
+        this.uwaga = uwaga;
+    }
+
+    public int getAddUserId() {
+        return addUserId;
+    }
+
+    public void setAddUserId(int addUserId) {
+        this.addUserId = addUserId;
+    }
+
+    public DateTime getAddDate() {
+        return addDate;
+    }
+
+    public void setAddDate(DateTime addDate) {
+        this.addDate = addDate;
+    }
+
+    // SQL QUERY: Creating the Table
+    public static final String CREATE_TABLE =
+            "CREATE TABLE " + TABLE_NAME + " (" +
+                    COLUMN_ID + " INTEGER PRIMARY KEY, " +
+                    COLUMN_MASZYNAID + " INTEGER, " +
+                    COLUMN_OPERATORID + " INTEGER, " +
+                    COLUMN_PRZEWINIENIEID + " INTEGER, " +
+                    COLUMN_SMS + " BIT, " +
+                    COLUMN_SMSNUMER + " INTEGER, " +
+                    COLUMN_SMSTRESC + " VARCHAR, " +
+                    COLUMN_UWAGA + " VARCHAR, " +
+                    COLUMN_ADDUSERID + " INTEGER, " +
+                    COLUMN_ADDDATE + " DATETIME, " +
+                    "FOREIGN KEY " + "(" + COLUMN_MASZYNAID + ") " +
+                    "REFERENCES " + "MASZYNY" + "(" + COLUMN_MASZYNAID+ ")" +
+                    "FOREIGN KEY " + "(" + COLUMN_OPERATORID + ") " +
+                    "REFERENCES " + "OPERATOR" + "(" + COLUMN_OPERATORID+ ")" +
+                    "FOREIGN KEY " + "(" + COLUMN_PRZEWINIENIEID + ") " +
+                    "REFERENCES " + "PRZEWINIENIE" + "(" + COLUMN_PRZEWINIENIEID+ ")" +
+                    ")";
+
 }
