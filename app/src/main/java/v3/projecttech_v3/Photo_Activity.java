08a469a2fca08d47abcf2c74cb2bcecc15c88b00
Remeklago <remigiusz.lagodzki@gmail.com>
2023-01-20 11:28:45 +0100

    add mini images for testing input images from main photo activity

--- app/src/main/java/v3/projecttech_v3/Photo_Activity.java
+++ app/src/main/java/v3/projecttech_v3/Photo_Activity.java
@@ -15,17 +15,37 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+
+import v3.projecttech_v3.formularze.Formularz5_Maszyna_Pracownik_Skarga;
+
 public class Photo_Activity extends AppCompatActivity {
 
     ImageView imageViewPhoto;
     Button buttonPhoto;
+    Button buttonPhotoZatwierdz;
+    public static ArrayList<Bitmap> bitmapsPhotos;
+    public static ArrayList<String> bytesPhotos;
+    ImageView imageViewMini1;
+    ImageView imageViewMini2;
+    ImageView imageViewMini3;
+    ImageView imageViewMini4;
+
+
+
     public static final int CAMERA_ACTION_CODE = 1;
     ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
@@ -38,6 +58,20 @@ public class Photo_Activity extends AppCompatActivity {
                     if (requestCode == RESULT_OK && data != null) {
                         Bitmap bitmapPhoto = (Bitmap) data.getExtras().get("data");
                         imageViewPhoto.setImageBitmap(bitmapPhoto);
+
+                        bitmapsPhotos.add(bitmapPhoto);
+                        Log.i("Checking", "Bitmaps size " + bitmapsPhotos.size());
+
+                        if (Formularz5_Maszyna_Pracownik_Skarga.images.size() > 0) {
+                            imageViewMini1.setImageBitmap(bitmapPhoto);
+                        }
+
+                        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                        bitmapPhoto.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+                        byte[] byteArrayImage = byteArrayOutputStream .toByteArray();
+                        String encodedByteArrayImage = Base64.encodeToString(byteArrayImage, Base64.DEFAULT);
+                        bytesPhotos.add(encodedByteArrayImage);
+
                     }
                 }
             }
@@ -51,6 +85,19 @@ public class Photo_Activity extends AppCompatActivity {
 
         imageViewPhoto = findViewById(R.id.imageViewPhoto);
         buttonPhoto = findViewById(R.id.buttonPhoto);
+        buttonPhotoZatwierdz = findViewById(R.id.buttonPhotoZatwierdz);
+        imageViewMini1 = findViewById(R.id.imageViewMini1);
+        imageViewMini2 = findViewById(R.id.imageViewMini2);
+        imageViewMini3 = findViewById(R.id.imageViewMini3);
+        imageViewMini4 = findViewById(R.id.imageViewMini4);
+
+        bitmapsPhotos = new ArrayList<Bitmap>();
+
+        imageViewMini1.setImageResource(R.drawable.ic_photo_size_select_large_24);
+        imageViewMini2.setImageResource(R.drawable.ic_photo_size_select_large_24);
+        imageViewMini3.setImageResource(R.drawable.ic_photo_size_select_large_24);
+        imageViewMini4.setImageResource(R.drawable.ic_photo_size_select_large_24);
+
 
         // Request for camera runtime permission
         if (ContextCompat.checkSelfPermission(Photo_Activity.this, Manifest.permission.CAMERA)
@@ -60,28 +107,32 @@ public class Photo_Activity extends AppCompatActivity {
             }, CAMERA_ACTION_CODE);
         }
 
+//        final int[] howManyClicks = {0};
+
         buttonPhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intentMakePhoto = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
                 if (intentMakePhoto.resolveActivity(getPackageManager()) != null) {
 //                    startActivityForResult(intentMakePhoto, CAMERA_ACTION_CODE);
                     activityResultLauncher.launch(intentMakePhoto);
+//                    howManyClicks[0]++;
                 } else {
                     Toast.makeText(Photo_Activity.this, "There is no app that support this action", Toast.LENGTH_SHORT).show();
                 }
             }
         });
+
+        buttonPhotoZatwierdz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentGoToFormularz = new Intent(Photo_Activity.this, Formularz5_Maszyna_Pracownik_Skarga.class);
+                intentGoToFormularz.putStringArrayListExtra("imagesByte", bytesPhotos);
+                startActivity(intentGoToFormularz);
+            }
+        });
+
      }
-
-//        @Override
-//        protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-//            super.onActivityResult(requestCode, resultCode, data);
-//            if (requestCode == CAMERA_ACTION_CODE && resultCode == RESULT_OK && data != null) {
-//                Bitmap bitmapPhoto = (Bitmap) data.getExtras().get("data");
-//                imageViewPhoto.setImageBitmap(bitmapPhoto);
-//            }
-//        }
-
 
 }
