1ef74bc89b87b26e925fae3dcb4c08589fd3de74
Remeklago <remigiusz.lagodzki@gmail.com>
2023-02-06 08:36:03 +0100

    changes related with new layout, delete few input variables add SharedPreferences and dataBaseChanges object

--- app/src/main/java/v3/projecttech_v3/Start/Activity_User_Register.java
+++ app/src/main/java/v3/projecttech_v3/Start/Activity_User_Register.java
@@ -22,6 +22,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -30,19 +31,17 @@ import v3.projecttech_v3.R;
 import v3.projecttech_v3.db.entity.Data5;
 
 public class Activity_User_Register extends AppCompatActivity {
-    TextView inputImie;
-    TextView inputNazwisko;
-    TextView inputEmail;
-    TextView inputTelefon;
-    TextView inputLokalizacja;
-    TextView inputDzial;
-    TextView inputStanowisko;
-    TextView inputHaslo;
-    TextView inputHasloPotwierdz;
+    public static TextView inputImie;
+    public static TextView inputNazwisko;
+    public static TextView inputEmail;
+    public static TextView inputTelefon;
     TextView textViewRejestracja;
     TextView textViewMaszJuzKonto;
     public static Data5 tmpInsertData;
     public static String tmpstatusID;
+    public static DataBaseChanges_Register dataBaseChanges_register;
+    HashMap<String, String> database;
+    public static int userLoginCheck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,25 +56,39 @@ public class Activity_User_Register extends AppCompatActivity {
         textViewRejestracja = findViewById(R.id.textViewRejestracja);
         textViewMaszJuzKonto = findViewById(R.id.textViewMaszJuzKonto);
 
-        ImageView imageLogo2 = findViewById(R.id.imageLogo2);
+        ImageView imageLogo = findViewById(R.id.imageLogo);
         Button buttonRejestracja = findViewById(R.id.buttonRejestracja);
 
+        dataBaseChanges_register = new DataBaseChanges_Register();
+        Intent intentVerification = new Intent(Activity_User_Register.this, Activity_User_Verification.class);
 
+        SharedPreferences preferencesLogin = getSharedPreferences("ShearedUserLogin", MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferencesLogin.edit();
+        editor.putInt("userLoginCheck", 0);
+        editor.apply();
+        userLoginCheck = preferencesLogin.getInt("userLoginCheck",0);
+        Log.i("checking", "statusLogin1: " + userLoginCheck);
 
         buttonRejestracja.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 //                Boolean isAllFieldsValidateTrue = CheckAllFields();
-                takingValuesFromInputTextView();
-                Intent intentRegister = new Intent(Activity_User_Register.this, Activity_User_Login_oldversion.class);
+//                takingValuesFromInputTextView();
 
                 if (CheckAllFields()) {
-                    dataBaseSQLUser.insertData(tmpInsertData);
-                    Toast.makeText(getApplicationContext(), "Rejestracja przebiegła pomyślnie", Toast.LENGTH_LONG).show();
+                    database = dataBaseChanges_register.doInBackground();
+                    Log.i("checking","database1: " + database.get("Status"));
+                    Log.i("checking","database1: " + database.get("Komunikat"));
 
-                    startActivity(intentRegister);
+                    if (database.get("Status").equals("1")) {
+                        Toast.makeText(getApplicationContext(), database.get("Komunikat"), Toast.LENGTH_LONG).show();
+                        startActivity(intentVerification);
+                    } else {
+                        Toast.makeText(getApplicationContext(), database.get("Komunikat"), Toast.LENGTH_LONG).show();
+                    }
+
                 } else {
-                    Toast.makeText(getApplicationContext(), "Wprowadzone dane są niepoprawne", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "Rejestracja nie powiodła się", Toast.LENGTH_LONG).show();
                 }
             }
         });
@@ -83,51 +96,15 @@ public class Activity_User_Register extends AppCompatActivity {
         textViewMaszJuzKonto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intentMamJuzKonto = new Intent(Activity_User_Register.this, Activity_User_Login_oldversion.class);
+                Intent intentMamJuzKonto = new Intent(Activity_User_Register.this, Login_User_activity.class);
                 startActivity(intentMamJuzKonto);
             }
         });
     }
 
-
-
-    // taking values from input textviews
-    public Data5 takingValuesFromInputTextView() {
-//            String imie = inputImie.getText().toString();
-//            String nazwisko = inputNazwisko.getText().toString();
-//            String email = inputEmail.getText().toString();
-//            String telefon = inputTelefon.getText().toString();
-//            String lokalizacja = inputLokalizacja.getText().toString();
-//            String dzial = inputDzial.getText().toString();
-//            String stanowisko = inputStanowisko.getText().toString();
-//            String haslo = inputHaslo.getText().toString();
-//            String hasloPotwierdz = inputHasloPotwierdz.getText().toString();
-        tmpstatusID = "1";
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            tmpInsertData = new Data5(
-                    inputImie.getText().toString(),
-                    inputNazwisko.getText().toString(),
-                    inputEmail.getText().toString(),
-                    inputTelefon.getText().toString(),
-                    inputLokalizacja.getText().toString(),
-                    inputDzial.getText().toString(),
-                    inputStanowisko.getText().toString(),
-                    LocalDateTime.now().toString(),             // addDate
-                    tmpstatusID,                 // statusID
-                    inputHaslo.getText().toString(),
-                    inputHasloPotwierdz.getText().toString()
-            );
-        }
-        return tmpInsertData;
-    }
-
-
     private boolean CheckAllFields() {
 
-        if (validateImie() && validateNazwisko() && inputEmail() && inputTelefon() &&
-                inputLokalizacja() && inputDzial() && inputStanowisko() && inputHaslo() &&
-                inputHasloPotwierdz()) {
+        if (validateImie() && validateNazwisko() && inputEmail() && inputTelefon()) {
             // after all validation return true.
             return true;
         } else {
@@ -157,7 +134,7 @@ public class Activity_User_Register extends AppCompatActivity {
 //            Pattern patternEmail = Pattern.compile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$");
         Pattern patternEmail = Pattern.compile("^[A-Za-z0-9+_.-]+@(.+)$");
 
-        Matcher matcherEmail = patternEmail.matcher(tmpInsertData.getMail().trim());
+        Matcher matcherEmail = patternEmail.matcher(inputEmail.getText().toString());
 
         String mailDatabaseInput = null;
         if (inputEmail.length() == 0) {
@@ -186,85 +163,7 @@ public class Activity_User_Register extends AppCompatActivity {
             return true;
         }}
 
-    public boolean inputLokalizacja() {
-        if (inputLokalizacja.length() == 0) {
-            inputLokalizacja.setError("This field is required");
-            return false;
-        } else {
-            return true;
-        }}
 
-    public boolean inputDzial() {
-        if (inputDzial.length() == 0) {
-            inputDzial.setError("This field is required");
-            return false;
-        } else {
-            return true;
-        }}
-
-    public boolean inputStanowisko() {
-        if (inputStanowisko.length() == 0) {
-            inputStanowisko.setError("This field is required");
-            return false;
-        } else {
-            return true;
-        }}
-
-    public boolean inputHaslo() {
-        Pattern patternPassword = Pattern.compile(
-//                    "^" +
-//                    "(?=.*[0-9])" +         //at least 1 digit
-//                    "(?=.*[a-z])" +         //at least 1 lower case letter
-//                    "(?=.*[A-Z])" +         //at least 1 upper case letter
-//                    "(?=.*[a-zA-Z])" +      //any letter
-//                    "(?=.*[@#$%^&+=])" +    //at least 1 special character
-//                    "(?=S+$)" +           //no white spaces
-//                    ".{4,}" +               //at least 4 characters
-//                    "$"
-                "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$"
-        );
-        Matcher matcherPassword = patternPassword.matcher(tmpInsertData.getPassword().trim());
-
-        if (inputHaslo.length() == 0) {
-            inputHaslo.setError("Password is required");
-            return false;
-        } else if (!matcherPassword.matches()) {
-            inputHaslo.setError("Password should contain minimum 8 characters, 1 digit, " +
-                    "1 lower case letter, 1 upper case letter, 1 special character");
-            return false;
-        }
-        Log.i("checking", inputHaslo.toString());
-        return true;
-    }
-
-    public boolean inputHasloPotwierdz() {
-        Pattern patternPassword = Pattern.compile(
-//                    "^" +
-//                    "(?=.*[0-9])" +         //at least 1 digit
-//                    "(?=.*[a-z])" +         //at least 1 lower case letter
-//                    "(?=.*[A-Z])" +         //at least 1 upper case letter
-//                    "(?=.*[a-zA-Z])" +      //any letter
-//                    "(?=.*[@#$%^&+=])" +    //at least 1 special character
-//                    "(?=S+$)" +           //no white spaces
-//                    ".{4,}" +               //at least 4 characters
-//                    "$"
-
-                "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$"
-        );
-        Matcher matcherPassword = patternPassword.matcher(tmpInsertData.getPasswordConfirm());
-        if (inputHasloPotwierdz.length() == 0) {
-            inputHasloPotwierdz.setError("Password is required");
-            return false;
-        } else if (!matcherPassword.matches()) {
-            inputHasloPotwierdz.setError("Password should contain minimum 4 characters");
-            return false; }
-//            } else if (!inputHasloPotwierdz.toString().equals(inputHaslo.toString())) {
-//                inputHasloPotwierdz.setError("Check password, it must be the same");
-//                return false;
-//            }
-        Log.i("checking", inputHasloPotwierdz.toString());
-        return true;
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
